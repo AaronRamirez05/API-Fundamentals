12e0fedf6c48529adb45b402485e12d49bc5c4f2
AaronRamirez05 <aaronramirez879@yahoo.com>
2024-08-30 17:40:49 -0500

    Part 9 halfway done

--- CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21c9e62f8e9b20026b4e1e8dd536e87201cda93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcfd30660282856a73ada93bf2539bde519ec76")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
