a62a25bea07e154541effcaf37b50d9d339bb87e
AaronRamirez05 <aaronramirez879@yahoo.com>
2024-08-29 23:07:56 -0500

    Part 8 finished

--- CityInfo/CityInfo.API/Models/Services/ICityInfoRepository.cs
+++ CityInfo/CityInfo.API/Models/Services/ICityInfoRepository.cs
@@ -6,6 +6,7 @@ namespace CityInfo.API.Models.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCitiesAsync();
+        Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(string? name, string? searchQuery,int pageNumber, int pageSize);
         Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<bool> CityExistsAsync(int cityId);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestsForCityAsync(int cityId);
